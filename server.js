18a31e19415d48b687f43b840289311877785a64
Simran Gill <simrangill@simrans-air.mynetworksettings.com>
2025-05-27 21:06:52 -0700

    Initial commit for Express Navbar assignment

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route serves index.html with navbar
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route for Climate Crisis
+app.get('/climate-crisis', (req, res) => {
+  res.send('<h1>Climate Crisis</h1><p><a href="https://github.com/yourusername/climate-crisis-repo">View GitHub Repo</a></p>');
+});
+
+// Route for Typesetting
+app.get('/typesetting', (req, res) => {
+  res.send('<h1>Typesetting</h1><p><a href="https://github.com/yourusername/typesetting-repo">View GitHub Repo</a></p>');
+});
+
+// Route for Four Algorithms
+app.get('/four-algorithms', (req, res) => {
+  res.send('<h1>Four Algorithms</h1><p><a href="https://github.com/yourusername/four-algorithms-repo">View GitHub Repo</a></p>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
